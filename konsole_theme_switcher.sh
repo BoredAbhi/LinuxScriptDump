b48118f984d083fabe1f937e9b500dfc4eb3196c
Abhijeet Bhattacharjee <teejihba8@gmail.com>
2019-10-01 08:05:19 +0530

    adding comments

--- konsole_theme_switcher.sh
+++ konsole_theme_switcher.sh
@@ -6,7 +6,7 @@ BASHRC=/home/abhijeet/.bashrc
 echo $HOUR
 echo $BASHRC
 
-sed -i '$ d' $BASHRC
+sed -i '$ d' $BASHRC #deletes last line in bashrc ... in this case konsole color is set via bashrc LAST LINE.
 
 if [ $HOUR -gt 18 ] || [ $HOUR -lt 6 ]
 then
